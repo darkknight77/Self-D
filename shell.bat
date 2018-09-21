937a6b6fded9777de235a729f9a6863c323b1213
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-09-21 14:30:53 +0530

    selfD files

---
+++ shell.bat
@@ -0,0 +1,39 @@
+cd C:\Users\Latheef\Documents\Self-D
+
+git add .
+
+git commit -m "selfD files"
+
+git push -u origin master
+
+echo "Done Successfully"
+
+H:
+
+cd MyResume
+
+git add .
+
+git commit -m "MyResume files"
+
+git push -u origin master
+
+echo "Done Successfully"
+
+C:
+
+cd ../..
+
+cd workspace
+
+git add .
+
+git commit -m "workspace files"
+
+git push -u origin master
+
+echo "Done Successfully"
+
+pause
+
+ 
